8c41bb2895314cf95947c5b001beb08bbd058083
BuiniyYarik <sokoslav1707@gmail.com>
2024-05-03 04:27:17 +0300

    Modify file using pylint

--- scripts/build_projectdb.py
+++ scripts/build_projectdb.py
@@ -1,5 +1,10 @@
-import psycopg2 as psql
+"""
+This module handles the creation of tables, data import,
+and data processing for a PostgreSQL database.
+"""
+
 import os
+import psycopg2 as psql
 
 
 def check_table_exists_and_has_data(cursor, table_name):
@@ -11,74 +16,76 @@ def check_table_exists_and_has_data(cursor, table_name):
     - table_name: The name of the table to check.
 
     Returns:
-    - exists (bool): True if the table exists, False otherwise.
-    - has_data (bool): True if the table contains data, False otherwise.
+    - table_exists (bool): True if the table exists, False otherwise.
+    - table_has_data (bool): True if the table contains data, False otherwise.
     """
-    # Check if the specified table exists in the public schema
     cursor.execute(f"""
         SELECT EXISTS (
-            SELECT FROM information_schema.tables 
+            SELECT FROM information_schema.tables
             WHERE table_schema = 'public' AND table_name = '{table_name}');
     """)
-    exists = cursor.fetchone()[0]
+    table_exists = cursor.fetchone()[0]
 
-    # If the table exists, check if it has any data
-    if exists:
+    if table_exists:
         cursor.execute(f"SELECT EXISTS (SELECT 1 FROM {table_name} LIMIT 1);")
-        has_data = cursor.fetchone()[0]
-        return exists, has_data
-    return exists, False
+        table_has_data = cursor.fetchone()[0]
+        return table_exists, table_has_data
+    return table_exists, False
 
+
+# Define constants
+BASE_PATH = os.path.expanduser("~/bigdata-final-project")
+FILE_PATH = os.path.join(BASE_PATH, "secrets/.psql.pass")
+DATA_FILENAME = "Combined_Flights_2021.csv"
+HOSTNAME = 'hadoop-04.uni.innopolis.ru'
+PORT = '5432'
+USERNAME = 'team22'
+DATABASE_NAME = 'team22_projectdb'
 
 # Read password from secrets file
-file_path = os.path.join("../secrets", ".psql.pass")
-with open(file_path, "r") as file:
-    password = file.read().rstrip()
-
-
-# Define the name of the CSV file with data to import
-data_filename = "Combined_Flights_2021.csv"
-
+with open(FILE_PATH, "r", encoding="utf-8") as file:
+    PASSWORD = file.read().rstrip()
 
 # Build the connection string
-hostname = 'hadoop-04.uni.innopolis.ru'
-port = '5432'
-username = 'team22'
-database_name = 'team22_projectdb'
-conn_string = f"host={hostname} port={port} dbname={database_name} user={username} password={password}"
-
+CONN_STRING = f"host={HOSTNAME} port={PORT} \
+                dbname={DATABASE_NAME} user={USERNAME} password={PASSWORD}"
 
 # Connect to the PostgreSQL database
-with psql.connect(conn_string) as conn:
+with psql.connect(CONN_STRING) as conn:
     cur = conn.cursor()
 
     # Check if the 'flights' table exists and if it has data
     exists, has_data = check_table_exists_and_has_data(cur, 'flights')
 
     if not exists or not has_data:
-
         # Create tables if they do not exist
         print("Creating tables...")
-        with open(os.path.join("../sql", "create_tables.sql")) as file:
+        with open(
+               os.path.join(BASE_PATH, "sql/create_tables.sql"), "r", encoding="utf-8"
+        ) as file:
             content = file.read()
             cur.execute(content)
         conn.commit()
         print("Tables created successfully!")
 
-
         # Import data if the 'flights' table is empty
         print("Importing data to raw table...")
-        with open(os.path.join("../sql", "import_to_raw.sql")) as file:
+        with open(
+            os.path.join(BASE_PATH, "sql/import_to_raw.sql"), "r", encoding="utf-8"
+        ) as file:
             command = file.read()
-            with open(os.path.join("../data", data_filename), "r") as flights_raw:
+            with open(
+                os.path.join(BASE_PATH, "data", DATA_FILENAME), "r", encoding="utf-8"
+            ) as flights_raw:
                 cur.copy_expert(command, flights_raw)
         conn.commit()
         print("Raw data imported successfully!")
 
-
         # Process data from raw to production table
         print("Processing raw data to production table...")
-        with open(os.path.join("../sql", "process_to_prod.sql")) as file:
+        with open(
+            os.path.join(BASE_PATH, "sql/process_to_prod.sql"), "r", encoding="utf-8"
+        ) as file:
             content = file.read()
             cur.execute(content)
         conn.commit()
