87aea2e73fa3e77b8538f18a8ca6d76b98af0289
sofi12321 <sophialapin2421@gmail.com>
2024-05-12 10:02:32 +0300

    Edit stage 4 script

--- scripts/stage4.sh
+++ scripts/stage4.sh
@@ -1,1 +1,35 @@
 #!/bin/bash
+# Define base paths
+base_path=~/bigdata-final-project
+base_hdfs_path=/user/team22/project
+
+# Read the password to access the database
+password=$(head -n 1 $base_path/secrets/.hive.pass)
+
+# Clear files that will be stored
+echo "Clear files if exist"
+hdfs dfs -rm -r -f $base_hdfs_path/hive/warehouse/hyperparameters > /dev/null 2>&1
+hdfs dfs -rm -r -f $base_hdfs_path/hive/warehouse/prediction_samples > /dev/null 2>&1
+hdfs dfs -rm -r -f $base_hdfs_path/hive/warehouse/optimization > /dev/null 2>&1
+echo "Finished clearing"
+
+echo "Load ML modelling results"
+hdfs dfs -cp -f $base_hdfs_path/output/hyperparameters/ $base_hdfs_path/hive/warehouse/
+hdfs dfs -cp -f $base_hdfs_path/output/prediction_samples/ $base_hdfs_path/hive/warehouse/
+hdfs dfs -cp -f $base_hdfs_path/output/optimization/ $base_hdfs_path/hive/warehouse/
+# > /dev/null 2>&1
+
+# Load ML modelling results into Hive tables
+echo "Create Hive table with hyperparameters info"
+beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team22 -p $password -f $base_path/sql/st4_hyperparameters_create_table.hql --hiveconf hive.resultset.use.unique.column.names=false > /dev/null 2>&1
+echo "Finished loading hyperparameters info"
+
+echo "Create Hive table with prediction samples"
+beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team22 -p $password -f $base_path/sql/st4_prediction_samples_create_table.hql --hiveconf hive.resultset.use.unique.column.names=false > /dev/null 2>&1
+echo "Finished loading prediction samples"
+
+echo "Create Hive table with optimization results"
+beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team22 -p $password -f $base_path/sql/st4_optimization_results_create_table.hql --hiveconf hive.resultset.use.unique.column.names=false > /dev/null 2>&1
+echo "Finished loading optimization results"
+
+echo "Finished loading ML results"
